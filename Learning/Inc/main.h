b0477d4fdc02a460851cc53c99db0cf48bec4925
MATH-286-Dell-Ubuntu <jhu114514@gmail.com>
2025-05-18 20:54:46 +0200

    临时更新

--- Learning/Inc/main.h
+++ Learning/Inc/main.h
@@ -2,11 +2,32 @@
 #define __main_h__
 
 #include <iostream>
+#include <bitset>
+#include <string>
 
 void Hello_World(){
 
   std::cout << "Hello World!" << std::endl;
 }
 
+
+void PRINT(int data,
+           std::string type){
+  
+  if (type == "0b"){
+    std::cout << std::bitset<16>(data) << std::endl;  
+  }
+
+  else if (type == "0x"){
+    std::cout << std::hex << data << std::endl;  
+  }
+  else if (type == "dec"){
+    std::cout << std::dec << data << std::endl;  
+  }
+  else{
+    std::cout << "Error: Unknown type" << std::endl;
+  }
+
+}
 
 #endif
--- Learning/Src/main.cpp
+++ Learning/Src/main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <bitset>
 #include "main.h"
+#include <array>
 using namespace std;
+
+
+struct student_data{
+  int ID;
+  string name;
+};
+
 
 int main(){
   Hello_World();
 
-  int a = 0b00001;
+  int a = 0b00000011;
   int b = a << 4;
 
-  cout << a << endl;
-  cout << b << endl;
+  int data1 = 0b11001100;
+  int data2 = 0b10101010;
 
-  printf("a = %d\n",a);
-  printf("b = %d\n",b);
+  int data = data1 << 8 | data2;  
+
+  PRINT(data, "0b");
+  PRINT(data1 & data2, "0b");
+  PRINT(data1 | data2, "0b");
+
+  // struct student_data student1;
+  student_data student1;
+  student1.ID = 1;
+  student1.name = "John Doe";
+
+return 0;
+
 }
